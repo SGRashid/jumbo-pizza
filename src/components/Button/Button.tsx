8dc504ab0bd2ab414f93806e67c340198b0314b0
Sergei Gabdullin <sgrashid@yandex.ru>
2025-10-27 15:19:44 +0500

    кнопка

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,17 @@
 import { type JSX } from 'react';
+import classNames from 'classnames/bind';
 import { type ButtonProps } from './Button.props';
-import './Button.css';
+import styles from './Button.module.css';
 
 const Button = ({ children, ...props}: ButtonProps): JSX.Element => {
+    const cx = classNames.bind(styles);
+    const btnClasses = cx({
+        button: true,
+        primary: true
+    });
     return (
         <button
-            className='button primary'
+            className={ btnClasses }
             { ...props }
         >
             { children }
