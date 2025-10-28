931012957b6dba51001b9eb8dc3cc9350a1f9f07
Sergei Gabdullin <sgrashid@yandex.ru>
2025-10-28 23:23:33 +0500

    роутинг работает

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { useState, type JSX, type MouseEvent } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { ButtonPrimary } from './components/ButtonPrimary/ButtonPrimary';
 import Fly from './components/Fly/Fly';
 import { ButtonSecondary } from './components/ButtonSecondary/ButtonSecondary';
 import { ButtonAccent } from './components/ButtonAccent/ButtonAccent';
 import { Input } from './components/Input/Input';
+import { Menu } from './components/pages/Menu/Menu';
+import { Cart } from './components/pages/Cart/Cart';
 
 function App(): JSX.Element {
   const [fliesWereKilled, setNumberOfKilledFlies] = useState<number>(0);
@@ -25,11 +28,14 @@ function App(): JSX.Element {
       <Input
         value={inputValue}
         onChange={handleChange}
-
       />
       <p>input value: {inputValue}</p>
       <p> Мух убито: { fliesWereKilled }</p>
       <Fly onSquash={handleFlySquashed}></Fly>
+      <Routes>
+        <Route path='/' element={<Menu></Menu>} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/pages/Menu/Menu.tsx
@@ -0,0 +1,8 @@
+import { Link } from 'react-router-dom';
+
+export const Menu = () => (
+    <>
+        Menu
+        <Link to='/cart'>cart</Link>
+    </>
+);
