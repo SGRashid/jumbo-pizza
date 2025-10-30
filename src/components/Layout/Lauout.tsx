726c5d85cddf66d3708cede4a64c7a828dd61fa3
Sergei Gabdullin <sgrashid@yandex.ru>
2025-10-30 15:45:12 +0500

    отлеживаем роут

--- src/components/Layout/Lauout.tsx
+++ src/components/Layout/Lauout.tsx
@@ -1,5 +1,5 @@
-import type { FC } from 'react';
-import { Outlet } from 'react-router-dom';
+import { useEffect, type FC } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import styles from './Layout.module.css';
 import { MenuLinks } from '../pages/MenuLinks/MenuLinks';
 import Fly from '../Fly/Fly';
@@ -7,16 +7,22 @@ import { ButtonSecondary } from '../ButtonSecondary/ButtonSecondary';
 import { FlyLabel } from '../FlyLabel/FlyLabel';
 import { MenuItems } from './Layout.constants';
 
-export const Layout: FC = () => (
-    <div className={ styles.layout }>
-        <div className={ styles.sidebar }>
-            <FlyLabel />
-            <MenuLinks items={ MenuItems } />
-            <ButtonSecondary>Выйти</ButtonSecondary>
-            <Fly />
+export const Layout: FC = () => {
+    const location = useLocation();
+
+    useEffect(() => console.log(location), [location]);
+    
+    return (
+        <div className={ styles.layout }>
+            <div className={ styles.sidebar }>
+                <FlyLabel />
+                <MenuLinks items={ MenuItems } />
+                <ButtonSecondary>Выйти</ButtonSecondary>
+                <Fly />
+            </div>
+            <div className={ styles['main-content'] }>
+                <Outlet />
+            </div>
         </div>
-        <div className={ styles['main-content'] }>
-            <Outlet />
-        </div>
-    </div>
-);
+    );
+};
