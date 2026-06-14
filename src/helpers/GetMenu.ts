9503575395868c90a7a49e15410816ecc5d5ecd7
Sergei Gabdullin <sgrashid@yandex.ru>
2026-06-14 21:07:02 +0500

    установил аксиос

--- src/helpers/GetMenu.ts
+++ src/helpers/GetMenu.ts
@@ -1,27 +1,15 @@
 import { type MenuItem } from '../interfaces/Menu.interface';
-import { httpErrorHandler } from '../helpers/HttpErrorHandler';
+// import { httpErrorHandler } from '../helpers/HttpErrorHandler';
 import { PREFIX } from '../assets/constants/Prefix.constants';
+import axios from 'axios';
 
 export const getMenu = async (): Promise<MenuItem[]> => {
     try {
-        const res = await fetch(`${PREFIX}/products`);
-        if (!res.ok) {
-            httpErrorHandler(res.status);
-            throw new Error(`HTTP error! status: ${res.status}`);
-        }
-        const data = await res.json();
-        console.log(data);
-        return data;
+        const res = await axios.get(`${PREFIX}/products`);
+        return res.data;
     } catch (err: Error | unknown) {
         console.error(err instanceof Error ? err.message : 'An unknown error occurred');
         throw err;
-    };
+    }
 };
 
-// export const getMenu = (): Promise<MenuItem[]> => fetch('https://purpleschool.ru/pizza-api-demo/products')
-//     .then((res) => {
-//         if (!res.ok) {
-//             throw new Error(`HTTP error! status: ${res.status}`);
-//         }
-//         return res.json() as Promise<MenuItem[]>;
-//     });
